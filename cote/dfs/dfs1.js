4b9d9ad2a67e63a3e27dcdf6d357f09cc7223b37
smkim <ksm0057@naver.com>
2023-04-11 15:39:35 +0900

    changes

---
+++ cote/dfs/dfs1.js
@@ -0,0 +1,30 @@
+// 4 6
+// 101111
+// 101010
+// 101011
+// 111011
+
+const exArr = [
+  [1, 0, 1, 1, 1, 1],
+  [1, 0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1, 1],
+  [1, 1, 1, 0, 1, 1],
+];
+
+function makeArray() {}
+
+let stack = [];
+
+let movingX = 0;
+let movingY = 0;
+let movingT = 0;
+
+function solution(g, s) {
+  stack.push(exArr[(0, 0)]);
+  // while(){
+
+  // }
+  return movingT;
+}
+
+console.log(solution(4, 6));
