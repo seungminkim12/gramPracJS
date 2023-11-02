5ced786b5922d99237f5f2046c53bb2ade9e5030
smkim <ksm0057@naver.com>
2023-11-02 21:16:13 +0900

    Feat: 횡스크롤 좌우 스크롤 버튼 추가

--- ValidationTest/index.js
+++ ValidationTest/index.js
@@ -1,74 +1,161 @@
-// var cnt = 0
-//         var cnt2 = 0
-//         var tmp = ''
-//         var tmp2 = ''
-//         var tmp3 = ''
-//         for (var i = 0; i < data.length; i++) {
-//           // charAt(): 문자값 반환
-//           tmp = data.charAt(i)
-//           tmp2 = data.charAt(i + 1)
-//           tmp3 = data.charAt(i + 2)
+// const testValidation = (data) => {
+//   let cnt1 = 0, cnt3 = 0;
 
-//           // charCodeAt(): 유니코드값 반환
-//           if (tmp.charCodeAt(0) - tmp2.charCodeAt(0) === 1 && tmp2.charCodeAt(0) - tmp3.charCodeAt(0) === 1) {
-//             cnt = cnt + 1
-//           }
-//           if (tmp.charCodeAt(0) - tmp2.charCodeAt(0) === -1 && tmp2.charCodeAt(0) - tmp3.charCodeAt(0) === -1) {
-//             cnt2 = cnt2 + 1
-//           }
-//         }
-//         if (cnt > 0 || cnt2 > 0) {
-//           uMessage.showSingleAlertBox('알림', <span class="ti ti-alert-circle error-alert-msg">&nbsp;연속된 문자를 3개 이상 사용하실 수 없습니다.<br></br>(ex: 123, 321, abc, cba 포함 불가)</span>)
-//           return false
-//         }
+//   const checkAsciiBetweenWord = (first, second, third) => {
+//     const diff1 = first.charCodeAt(0) - second.charCodeAt(0);
+//     const diff2 = second.charCodeAt(0) - third.charCodeAt(0);
 
-const testValidation = (data) => {
-  let cnt1 = 0,
-    cnt2 = 0,
-    cnt3 = 0;
-  let temp,
-    temp2,
-    temp3 = "";
+//     // Check for ascending consecutive characters
+//     if (diff1 === -1 && diff2 === -1) {
+//       console.log("Ascending consecutive characters:");
+//       console.log("Char 1:", first);
+//       console.log("Char 2:", second);
+//       console.log("Char 3:", third);
+//       cnt1++;
+//     }
 
-  for (let i in data) {
-    temp = data.charAt(+i);
-    temp2 = data.charAt(+i + 1);
-    temp3 = data.charAt(+i + 2);
+//     // Check for repeated characters
+//     if (diff1 === 0 && diff2 === 0) {
+//       console.log("Repeated characters:");
+//       console.log("Char 1:", first);
+//       console.log("Char 2:", second);
+//       console.log("Char 3:", third);
+//       cnt3++;
+//     }
 
-    // 정방향 연속성 case abc, bcd, 123, 567 ...
-    if (
-      temp.charCodeAt(0) - temp2.charCodeAt(0) === -1 &&
-      temp2.charCodeAt(0) - temp3.charCodeAt(0) === -1
-    ) {
-      console.log("temp", temp);
-      console.log("temp2", temp2);
-      console.log("temp3", temp3);
-      cnt1 = cnt1 + 1;
-    }
-    //
-    if (
-      temp.charCodeAt(0) - temp2.charCodeAt(0) === 1 &&
-      temp2.charCodeAt(0) - temp3.charCodeAt(0) === 1
-    ) {
-      console.log("temp", temp);
-      console.log("temp2", temp2);
-      console.log("temp3", temp3);
-      cnt2 = cnt2 + 1;
-    }
-    //반복되는 case
-    if (
-      temp.charCodeAt(0) - temp2.charCodeAt(0) === 0 &&
-      temp2.charCodeAt(0) - temp3.charCodeAt(0) === 0
-    ) {
-      console.log("temp", temp);
-      console.log("temp2", temp2);
-      console.log("temp3", temp3);
-      cnt3 = cnt3 + 1;
+//     return [diff1, diff2];
+//   };
+
+//   for (let i = 0; i < data.length - 2; i++) {
+//     const [diff1, diff2] = checkAsciiBetweenWord(
+//       data.charAt(i),
+//       data.charAt(i + 1),
+//       data.charAt(i + 2)
+//     );
+//   }
+
+//   console.log("cnt1", cnt1);
+//   console.log("cnt3", cnt3);
+// };
+
+// const testValidation = (data) => {
+//   let cnt1 = 0,
+//     // cnt2 = 0,
+//     cnt3 = 0;
+
+//   const checkAsciiBetweenWord = (first, second) => {
+//     return first.charCodeAt(0) - second.charCodeAt(0);
+//   };
+
+//   for (let i in data) {
+//     const firstChar = data.charAt(+i);
+//     const secondChar = data.charAt(+i + 1);
+//     const thirdChar = data.charAt(+i + 2);
+
+//     // 정방향 연속성 case abc, bcd, 123, 567 ...
+//     if (
+//       checkAsciiBetweenWord(firstChar, secondChar) === -1 &&
+//       checkAsciiBetweenWord(secondChar, thirdChar) === -1
+//     ) {
+//       console.log("temp", firstChar);
+//       console.log("temp2", temp2);
+//       console.log("temp3", temp3);
+//       cnt1++;
+//     }
+//     //
+//     // if (
+//     //   checkAsciiBetweenWord(temp, temp2) === 1 &&
+//     //   checkAsciiBetweenWord(temp2, temp3) === 1
+//     // ) {
+//     //   console.log("temp", temp);
+//     //   console.log("temp2", temp2);
+//     //   console.log("temp3", temp3);
+//     //   cnt2 = cnt2 + 1;
+//     // }
+//     //반복되는 case
+//     if (
+//       checkAsciiBetweenWord(firstChar, secondChar) === 0 &&
+//       checkAsciiBetweenWord(secondChar, thirdChar) === 0
+//     ) {
+//       console.log("temp", firstChar);
+//       console.log("temp2", secondChar);
+//       console.log("temp3", thirdChar);
+//       cnt3++;
+//     }
+//   }
+//   console.log("cnt1", cnt1);
+//   // console.log("cnt2", cnt2);
+//   console.log("cnt3", cnt3);
+// };
+
+// const testValidation = (data) => {
+//   let cnt1 = 0,
+//     cnt2 = 0,
+//     cnt3 = 0;
+
+//   const checkAsciiBetweenWord = (first, second) => {
+//     return first.charCodeAt(0) - second.charCodeAt(0);
+//   };
+
+//   for (let i in data) {
+//     const temp = data.charAt(+i);
+//     const temp2 = data.charAt(+i + 1);
+//     const temp3 = data.charAt(+i + 2);
+
+//     // 정방향 연속성 case abc, bcd, 123, 567 ...
+//     if (
+//       checkAsciiBetweenWord(temp, temp2) === -1 &&
+//       checkAsciiBetweenWord(temp2, temp3) === -1
+//     ) {
+//       cnt1 = cnt1 + 1;
+//     }
+//     //
+//     if (
+//       checkAsciiBetweenWord(temp, temp2) === 1 &&
+//       checkAsciiBetweenWord(temp2, temp3) === 1
+//     ) {
+//       cnt2 = cnt2 + 1;
+//     }
+//     //반복되는 case
+//     if (
+//       checkAsciiBetweenWord(temp, temp2) === 0 &&
+//       checkAsciiBetweenWord(temp2, temp3) === 0
+//     ) {
+//       cnt3 = cnt3 + 1;
+//     }
+//   }
+// };
+
+function isSameContinuedValue(value) {
+  var temp = "";
+
+  var intCnt = 0;
+
+  for (var i = 0; i < value.length; i++) {
+    temp = value.charAt(i);
+    console.log("temp", temp);
+    console.log("value.charAt(i + 1): ", value.charAt(i + 1));
+    console.log("value.charAt(i + 2): ", value.charAt(i + 2));
+    if (temp === value.charAt(i + 1) && temp === value.charAt(i + 2)) {
+      intCnt = intCnt + 1;
     }
   }
-  console.log("cnt1", cnt1);
-  console.log("cnt2", cnt2);
-  console.log("cnt3", cnt3);
-};
 
-testValidation("Eqbr5678@!");
+  if (intCnt > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * testcases
+Eqbr5678@!
+567cnEO#*
+rst927*!U
+666eonBW*!
+uuuKW529*!
+ */
+
+// testValidation("rst927*!U");
+console.log(isSameContinuedValue("11123"));
--- swiper-test/src/App.js
+++ swiper-test/src/App.js
@@ -1,12 +1,12 @@
 import logo from "../src/435.png";
-import { useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import "./App.css";
 import next from "../src/next_icon.jpg";
 
 function App() {
   const ref = useRef();
   const [isOverflow, setIsOverflow] = useState();
-  const [scrollState, setScrollState] = useState("right");
+  const [disableSrollState, setDisableScrollState] = useState("left");
   useLayoutEffect(() => {
     const { current } = ref;
     if (current) {
@@ -15,52 +15,137 @@ function App() {
     }
   }, [ref]);
 
+  const handleScroll = () => {
+    const { current } = ref;
+    console.log("current", current);
+    if (current) {
+      console.log("scrollWidth", current.scrollLeft);
+      console.log("current.scrollWidth", current.scrollWidth);
+      console.log("current.clientWidth", current.clientWidth);
+      console.log("current.offsetWidth", current.offsetWidth);
+      console.log(
+        "(current.scrollWidth - current.clientWidth) / 2",
+        current.scrollWidth - current.clientWidth
+      );
+
+      if (current.scrollLeft > 0) {
+        setDisableScrollState("");
+      }
+      if (
+        Math.ceil(current.scrollLeft) ===
+        current.scrollWidth - current.clientWidth
+      ) {
+        setDisableScrollState("right");
+      }
+      if (current.scrollLeft === 0) {
+        setDisableScrollState("left");
+      }
+      // if (
+      //   current.scrollLeft <=
+      //   (current.scrollWidth - current.clientWidth) / 8
+      // ) {
+      //   setDisableScrollState("right");
+      // } else {
+
+      // }
+    }
+  };
+
   const moveRight = () => {
     const { current } = ref;
     if (current) {
-      current.scrollLeft = current.clientWidth;
+      current.scrollLeft = current.scrollLeft + 35;
     }
   };
+
+  const moveLeft = () => {
+    const { current } = ref;
+    if (current) {
+      current.scrollLeft = current.scrollLeft - 35;
+    }
+  };
+
+  useEffect(() => {
+    const { current } = ref;
+    console.log("current", current.scrollLeft);
+    if (current) {
+      current.addEventListener("scroll", () => handleScroll(ref));
+    }
+    return () => {
+      if (current) {
+        window.removeEventListener("scroll", () => handleScroll(ref));
+      }
+    };
+  }, []);
   return (
     <div className="swiper">
-      <div className="swiper-container" ref={ref}>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
+      <div className="swiper-container">
+        {isOverflow && disableSrollState !== "left" ? (
+          <div className="prev-icon" onClick={moveLeft}>
+            <img src={next} alt="" />
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="swiper-wrapper" ref={ref}>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
+          <div className="swiper-item">
+            <img src={logo} alt="" />
+            <p>test</p>
+          </div>
         </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        <div className="swiper-item">
-          <img src={logo} alt="" />
-          <p>test</p>
-        </div>
-        {isOverflow ? (
+        {isOverflow && disableSrollState !== "right" ? (
           <div className="next-icon" onClick={moveRight}>
             <img src={next} alt="" />
           </div>
